c22339d565078c1edbcfa2cc44a40cf83c21db48
lsetiyawan <112475729+lsetiyawan@users.noreply.github.com>
2022-09-19 20:41:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("See documentation URL/api-docs/#/");
+  res.send("See documentation => URL/api-docs/#/");
 });
 
 app.use(userRouter);
